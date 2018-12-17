9449f62980bdd19d9590f6392cc06f502142b092
chickfilangel <43784681+chickfilangel@users.noreply.github.com>
2018-12-17 11:27:02 -0500

    Add files via upload

---
+++ lesson9/problem3.py
@@ -0,0 +1,8 @@
+print('-'*60)
+print('Alien Greeter Bot: ')
+password = input('What planet are you from? ')
+if password == 'Earth' :
+	print('HI UGLY! THOUGHT YOU WERE AN ALIEN!')
+else:
+	print('Welcome to Earth peanut head. ')
+print('-'*60)
---
+++ lesson9/problem4.py
@@ -0,0 +1,8 @@
+print('-'*60)
+print('Welcome to CLUB425, the most lit club in downtown ACTvF. Before you can enter, I need you to answer some questions... ')
+password = input('What is your age today? ')
+if password >= '21' :
+	print('Cool, come on in. ')
+else:
+	print('Youre gonna ned to back up. This club is 21+ only so find somewhere else to party or you find out what robot punches feel like.' )
+print('-'*60)
